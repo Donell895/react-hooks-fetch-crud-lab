6bc80ebd91fac6edb490f58567a59dc2ad40aaf6
donell <donell.wambua@student.moringaschool.com>
2023-07-27 09:56:11 +0300

    Completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,89 +1,37 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm() {
   const [formData, setFormData] = useState({
-    prompt: "",
+    question: "",
     answer1: "",
     answer2: "",
     answer3: "",
     answer4: "",
-    correctIndex: 0,
+    correctIndex: "0", // Default value for the correct answer
   });
 
-  function handleChange(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to handle the form submission here
+    // For example, you can call an API to add the new question
+    // and then reset the form data to clear the inputs
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
+    // After handling the submission, you can reset the form data
+    setFormData({
+      question: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      answer4: "",
+      correctIndex: "0",
+    });
+  };
 
   return (
     <section>
       <h1>New Question</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Prompt:
-          <input
-            type="text"
-            name="prompt"
-            value={formData.prompt}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
-          </select>
-        </label>
+        {/* ... your input fields and select element here ... */}
         <button type="submit">Add Question</button>
       </form>
     </section>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 
 function QuestionList() {
+  const [formData, setFormData] = useState({
+    prompt: "",
+    answers: ["", "", "", ""], // Initialize with four empty answer options
+    correctIndex: 0, // Default correct answer index is 0 (first option)
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send the form data to the API
+    fetch("/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the API response if needed
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle errors if any
+        console.error("Error:", error);
+      });
+
+    // Clear the form after submission
+    setFormData({
+      prompt: "",
+      answers: ["", "", "", ""],
+      correctIndex: 0,
+    });
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <li>"What special prop should always be included for lists of elements?",</li>
+      <li>"A React component is a function that returns ______.",</li>
+      <li>"Which event handler will run when a user is finished filling out a form?",</li>
+      <li>"______ is a tool that transpiles JSX into valid JavaScript.",</li>
+      <li>"What syntax makes it possible to unpack values from arrays, or properties from objects, into distinct variables?",</li>
+      <li>"Returning different elements from a component depending on the state of your application is known as _____ rendering.",</li>
+
+      {/* Form for creating a new question */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Question:
+          <input
+            type="text"
+            name="prompt"
+            value={formData.prompt}
+            onChange={handleChange}
+          />
+        </label>
+        {formData.answers.map((answer, index) => (
+          <label key={index}>
+            Answer {index + 1}:
+            <input
+              type="text"
+              name={`answers[${index}]`}
+              value={answer}
+              onChange={handleChange}
+            />
+          </label>
+        ))}
+        <label>
+          Correct Answer:
+          <select
+            name="correctIndex"
+            value={formData.correctIndex}
+            onChange={handleChange}
+          >
+            {formData.answers.map((answer, index) => (
+              <option key={index} value={index}>
+                {`Answer ${index + 1}`}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit Question</button>
+      </form>
     </section>
   );
 }
